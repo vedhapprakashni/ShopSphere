47f05c83b9217bcd4f0e2a581422617369158246
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-12-19 00:44:09 +0530

    almost done

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 const nextConfig = {
-  experimental: {
-    turbo: {
-      enabled: false,
-    },
-  },
   webpack(config: any, { dev }: { dev: boolean }) {
     if (dev) {
       config.devtool = false
